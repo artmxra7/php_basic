4bf0404b5245f5f060b0408043439b11c254870c
Erwin Rahayu <artmxare@gmail.com>
2019-03-13 02:37:13 +0700

    Add Author

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,19 @@
 <?php
-
+/**
+ * Basic.
+ *
+ * A simple BASIC written in PHP
+ *
+ * @author Erwin Rahayu aka ARTMX
+ *
+ * @version 1.0.0
+ *
+ * @copyright Copyright (c) 2019 Erwin Rahayu
+ * @license MIT License
+ **/
+/**
+ * The main Basic class.
+ *
+ * @author Erwin Rahayu
+ **/
 echo 'hello World';
--- phpvariable.php
+++ phpvariable.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Basic.
+ *
+ * A simple BASIC written in PHP
+ *
+ * @author Erwin Rahayu aka ARTMX
+ *
+ * @version 1.0.0
+ *
+ * @copyright Copyright (c) 2019 Erwin Rahayu
+ * @license MIT License
+ **/
+/**
+ * The main Basic class.
+ *
+ * @author Erwin Rahayu
+ **/
 $i = 1;
 $a = 2;
 $d = 3;
